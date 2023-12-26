1faf99812e98a3ae25134de171e6f3a6151dc4fa
LGoulart <leandrovgoulart@gmail.com>
2023-12-26 17:37:46 -0300

    first commit

---
+++ src/pages/App.js
@@ -0,0 +1,11 @@
+import gitlogo from '../assets/github.png'
+function App() {
+  return (
+    <div className="App">
+      <img src={gitlogo} width={72} height={72} alt='Logo Github' />
+      OLA
+    </div>  
+  );
+}
+
+export default App;
